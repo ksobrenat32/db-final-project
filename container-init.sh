9df3df0d12d74c4338342c2b4254edf2828bb741
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-01 19:16:19 -0600

    Add container-init.sh This scripts with the help of docker, starts an oracle database container  that also contains sqlplus client to connect to the database.

---
+++ container-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Database container from Oracle
+# https://container-registry.oracle.com/ords/ocr/ba/database/free
+
+docker run -d \
+    --name project-odb \
+    -p 0.0.0.0:1521:1521 \
+    -e ORACLE_PWD="system1" \
+    -v db-project:/opt/oracle/oradata \
+    container-registry.oracle.com/database/free:23.5.0.0-amd64
+
+# To connect, install SQL*Plus and run:
+#sqlplus sys/system1@//localhost:1521/FREE as sysdba
+# Or inside the container:
+#docker exec -it project-odb sqlplus sys/system1@//localhost:1521/FREE as sysdba
+
+
