28af28bc27790ac6639888d457213cff80af98a5
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-04 10:54:14 -0600

    Some ideas upgrade

--- scripts/s-03-tablas-temporales.sql
+++ scripts/s-03-tablas-temporales.sql
@@ -1,44 +1,35 @@
- --@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
+--@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
 --@Fecha creación: 1 de diciembre de 2024
---@Descripción: Arichivo con escenarios de tabla temporal
+--@Descripción: Archivo con escenarios de tabla temporal
 
+--Diseñar al menos un escenario donde se haga uso de una o más tablas temporales. Se pueden crear nuevas tablas en caso de ser necesario.
 
---Diseñar al menos un escenario donde se haga uso de una o más tablas temporales.
---Se pueden crear nuevas tablas en caso de ser necesario.
-
-
-
---EJEMPLO 1
 /*
-  Se desea ver los candidatos a una vacante de empleo para ser conductor.
-  una vez se termine de seleccionar al que se quedadara con el 
-  puesto se añadira a la lista de usuario_conductor
+  EJEMPLO 1
+  La plataforma donde se solicita el viaje, asigna el auto más cercano.
+  Para esto hace un listado de los autos más cercanos en la zona de las
+  3 categorías que existen. Estos se le presentan al usuario para que
+  elija el que más le convenga.
 */
-create global temporary table usuario_candidato_conductor(
-  nombre varchar2(200),
-  email  varchar2(100),
-  cv blob
- )on commit delete rows;
 
---EJEMPLO 2
-/*
-  La plataforma donde se solicita el viaje, asigna el auto màs cercano.
-  Para esto hace un listado de los autos de la zona. Tras conciderar
-  cual cumple con los requisitos del solicitante, se asignara al viaje
-*/
 create global temporary table vehiculo_candidato(
-  vehiculo_id number(10,0),
-  disponible boolean,
-  num_acientos number(1,0)
-)on commit preserve rows;
---EJEMPLO 3
+  vehiculo_id NUMBER(10,0),
+  num_asientos NUMBER(1,0),
+  categoria NUMBER(1,0)
+) on commit preserve rows;
+
 /*
-  El usuario desea añadir una tarjeta de credito, posee varias. De
-  forma predeterminada, se le muestran las tarjetas registradas en su
-  dispositivo. El puede decidir si agrega la tarjeta o no a su usuario
+  EJEMPLO 2
+  El usuario desea añadir una tarjeta de crédito al pago dentro de la
+  plataforma. El usuario puede tener varias tarjetas registradas en su
+  dispositivo, por lo que se le muestran todas las tarjetas que tiene
+  registradas para que elija la que desea añadir.
 */
+
 create private temporary table ora$ptt_tarj_pred(
-   num_tarjeta number(30,0),
-   banco   varchar2(20),
-   nombre  varchar2(200)
+  nombre_titular VARCHAR2(100),
+  banco VARCHAR2(100),
+  numero_tarjeta VARCHAR2(16),
+  mes NUMBER(2,0),
+  anio NUMBER(4,0),
 ); on commit drop definition;
