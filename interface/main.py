96c768009005a3fc2f3e89673cd619329d24a7f7
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-05 17:30:53 -0600

    Interface creation

---
+++ interface/main.py
@@ -0,0 +1,12 @@
+#@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
+#@Fecha creación: 4 de diciembre de 2024
+#@Descripción: Archivo principal de la interfaz gráfica
+
+import streamlit as st
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
