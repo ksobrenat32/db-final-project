11d0f7c3dc7e2c56bffad811fc24071801b5410f
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-04 19:27:29 -0600

    Add s-00 main script

---
+++ scripts/s-00-main.sql
@@ -0,0 +1,18 @@
+--@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
+--@Fecha creación: 1 de diciembre de 2024
+--@Descripción: Este llama a los scripts necesarios para crear todo.
+
+-- Crear usuarios
+@s-01-usuarios.sql
+
+-- Crear secuencias
+@s-05-secuencias.sql
+
+-- Crear entidades
+@s-02-entidades.sql
+
+-- Crear indices
+@s-06-indices.sql
+
+-- Crear sinónimos
+@s-07-sinonimos.sql
