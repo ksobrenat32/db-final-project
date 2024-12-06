0000129c6b4d86375703ef7501a8b611e97934fb
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-05 18:44:10 -0600

    Add scripts path to container

--- container-init.sh
+++ container-init.sh
@@ -8,6 +8,7 @@ docker run -d \
     -p 0.0.0.0:1521:1521 \
     -e ORACLE_PWD="system1" \
     -v db-project:/opt/oracle/oradata \
+    -v ./scripts:/home/oracle/scripts:Z \
     container-registry.oracle.com/database/free:23.5.0.0-amd64
 
 # To connect, install SQL*Plus and run:
