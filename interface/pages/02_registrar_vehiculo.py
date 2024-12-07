9cdbbefd79969f759447b225e7a2071d240c45c3
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-06 22:48:42 -0600

    Move database functions to the database.py file

--- interface/pages/02_registrar_vehiculo.py
+++ interface/pages/02_registrar_vehiculo.py
@@ -5,51 +5,6 @@
 from database import *
 import streamlit as st
 
-def get_all_usuario_conductor():
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT username FROM usuario WHERE ES_CONDUCTOR = 1")
-    r = []
-    for i in cursor.fetchall():
-        r.append(i[0])
-    return r
-
-def get_all_marca_from_categoria(categoria):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT descripcion FROM marca WHERE categoria = '{categoria}'")
-    r = []
-    for i in cursor.fetchall():
-        r.append(i[0])
-    return r
-
-def get_all_modelo_from_marca(marca):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT nombre FROM modelo WHERE marca_id = (SELECT marca_id FROM marca WHERE descripcion = '{marca}')")
-    r = []
-    for i in cursor.fetchall():
-        r.append(i[0])
-    return r
-
-def get_modelo_id(modelo):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT modelo_id FROM modelo WHERE nombre = '{modelo}'")
-    return cursor.fetchone()[0]
-
-def add_vehiculo(username, modelo, placas, anio):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO vehiculo (usuario_id, modelo_id, placas, anio) VALUES ((SELECT usuario_id FROM usuario WHERE username = '{username}'), (SELECT modelo_id FROM modelo WHERE nombre = '{modelo}'), '{placas}', '{anio}')")
-    conn.commit()
-
-def get_vehiculo_id(placas):
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT vehiculo_id FROM vehiculo WHERE placas = '{placas}'")
-    return cursor.fetchone()[0]
-
 # Variables
 
 if 'categoria' not in st.session_state:
