0be6ee53ce1b5cb795a4a72daf452dee18511ebd
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-08 12:52:25 -0600

    Fix s-11 tr-tarjeta_valida

--- scripts/s-11-tr-tarjeta_valida.sql
+++ scripts/s-11-tr-tarjeta_valida.sql
@@ -1,55 +1,30 @@
- --@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
+--@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
 --@Fecha creación: 1 de diciembre de 2024
---@Descripción: para verificar que la fecha de la tarjeta es valida
-
-
- --@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
---@Fecha creación: 1 de diciembre de 2024
---@Descripción: Trigger que se encargara de verificar que la tarjeta sea vigente
---se debe ingresar una nueva tarjeta vigente
---al actulizar, no se deben de cambiar los numeros de tarjeta ni el id, solo las fechas de vigencia,
---la cual, por supuesto, debe de estar actualmetne vigente
+--@Descripción: Trigger que se encargara de verificar que la tarjeta sea vigente. No es posible actualizar datos.
 
 CREATE OR REPLACE TRIGGER tr_tarjeta_valida
-  BEFORE INSERT OR 
+  BEFORE INSERT OR
   UPDATE
   ON tarjeta
 FOR EACH ROW
-
 DECLARE
---variables a utilizar
-
-v_fecha_actual DATE;
-v_fecha_tarjeta DATE;
-
-
-BEGIN 
---ASIGNANDO LA FECHA ACTUAL 
-v_fecha_actual := sysdate;
-v_fecha_tarjeta := last_day(to_date(:new.mes||'/'||:new.anio, 'mm/yy'));
+  v_fecha_actual DATE;
+  v_fecha_tarjeta DATE;
+BEGIN
+  --ASIGNANDO LA FECHA ACTUAL
+  v_fecha_actual := sysdate;
+  v_fecha_tarjeta := last_day(to_date(:new.mes||'/'||:new.anio, 'mm/yy'));
 
   CASE
   --CUANDO SE INSERTA UN NUEVO REGISTRO
-    WHEN INSERTING THEN 
-      IF v_fecha_actual > v_fecha_tarjeta THEN 
-      RAISE_APPLICATION_ERROR(-20020,'La tarjeta se encuentra actualmente vencida');
+    WHEN INSERTING THEN
+      IF v_fecha_actual > v_fecha_tarjeta THEN
+        RAISE_APPLICATION_ERROR(-20020,'La tarjeta se encuentra actualmente vencida');
       END IF;
-  --CUANDO SE ACTUALIZA UN REGISTRO YA EXISTENTE   
-    WHEN UPDATING THEN 
-      IF :old.tarjeta_id != :new.tarjeta_id or :old.numero_tarjeta != :new.numero_tarjeta THEN
-            RAISE_APPLICATION_ERROR(-20021,'Los identificadores de la tarjeta no coinciden');
-  --EN CASO DE NO SOLTAR ERROR, SE VERIFICARA QUE SE ACTUALIZE A UNA FECHA VIGENTE          
-      ELSE
-        IF v_fecha_actual > v_fecha_tarjeta THEN 
-           RAISE_APPLICATION_ERROR(-20020,'La tarjeta se encuentra actualmente vencida');
-           ELSE
-           --PERMITIR LA ACTUALIZACION DE DATOS
-           NULL;
-        END IF;
-      END IF;
+  --CUANDO SE ACTUALIZA UN REGISTRO YA EXISTENTE
+    WHEN UPDATING THEN
+      RAISE_APPLICATION_ERROR(-20020,'No es posible actualizar la tarjeta, crear una nueva');
     END CASE;
-
-
 END;
 /
 show errors
--- scripts/s-12-tr-tarjeta_valida-prueba.sql
+++ scripts/s-12-tr-tarjeta_valida-prueba.sql
@@ -1,22 +1,51 @@
+--@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
+--@Fecha creación: 1 de diciembre de 2024
+--@Descripción: Pruebas para el trigger tr_tarjeta_valida
+
 SET SERVEROUTPUT ON;
 
 DECLARE
   v_tarjeta_id NUMBER;
-
+  v_mes NUMBER;
+  v_anio NUMBER;
 BEGIN
-  -- Caso 1: Inserción correcta de una tarjeta
+  -- Caso 1: Inserción correcta de una tarjeta, se espera que no haya errores
   BEGIN
-    DBMS_OUTPUT.PUT_LINE('CASO INSERCION CORRECTA');
-    INSERT INTO tarjeta(tarjeta_id, numero_tarjeta, mes, anio, usuario_id)
-    VALUES (tarjeta_seq.NEXTVAL, 1122334455667788, 12, 24, 1);
+    DBMS_OUTPUT.PUT_LINE('CASO INSERCIÓN CORRECTA');
+    INSERT INTO tarjeta(numero_tarjeta, mes, anio, usuario_id)
+    VALUES (1122334455667788, 12, 24, 1);
+
+    -- Verificando que se haya insertado correctamente
+    SELECT tarjeta_id INTO v_tarjeta_id
+    FROM tarjeta
+    WHERE numero_tarjeta = 1122334455667788;
+
+    IF v_tarjeta_id IS NULL THEN
+      DBMS_OUTPUT.PUT_LINE('Error en la inserción correcta: No se encontró la tarjeta');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('Inserción correcta, tarjeta_id: ' || v_tarjeta_id);
+    END IF;
   EXCEPTION
     WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error en la insercion correcta: ' || SQLERRM);
+      DBMS_OUTPUT.PUT_LINE('Error en la inserción correcta: ' || SQLERRM);
   END;
 
-  -- Caso 2: Actualización correcta de una tarjeta
+  -- Caso 2: Inserción incorrecta de una tarjeta, se espera que haya un error
   BEGIN
-    DBMS_OUTPUT.PUT_LINE('CASO ACTUALIZACION CORRECTA');
+    DBMS_OUTPUT.PUT_LINE('CASO INSERCIÓN INCORRECTA');
+    INSERT INTO tarjeta(numero_tarjeta, mes, anio, usuario_id)
+    VALUES (1122334455667789, 12, 20, 1);
+
+    -- Si se pudo insertar correctamente, es un error pues no se puede insertar
+    DBMS_OUTPUT.PUT_LINE('Error en la inserción incorrecta: Se pudo insertar la tarjeta');
+  EXCEPTION
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('Excepción correcta');
+  END;
+
+  -- Caso 3: Actualización correcta de una tarjeta, se espera que haya un error
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('CASO ACTUALIZACIÓN');
     SELECT tarjeta_id INTO v_tarjeta_id
     FROM tarjeta
     WHERE numero_tarjeta = 1122334455667788;
@@ -24,34 +53,12 @@ BEGIN
     UPDATE tarjeta
     SET mes = 01, anio = 25
     WHERE tarjeta_id = v_tarjeta_id;
+
+    -- Si se pudo actualizar correctamente, es un error pues no se puede actualizar
+    DBMS_OUTPUT.PUT_LINE('Error en la actualización: Se pudo actualizar la tarjeta');
   EXCEPTION
     WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error en la actualizacion correcta: ' || SQLERRM);
-  END;
-
-  -- Caso 3: Inserción incorrecta de una tarjeta
-  BEGIN
-    DBMS_OUTPUT.PUT_LINE('CASO INSERCION INCORRECTA');
-    INSERT INTO tarjeta(tarjeta_id, numero_tarjeta, mes, anio, usuario_id)
-    VALUES (tarjeta_seq.NEXTVAL, 3333444433334444, 09, 24, 3);
-  EXCEPTION
-    WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error en la insercion incorrecta: ' || SQLERRM);
-  END;
-
-  -- Caso 4: Actualización incorrecta de una tarjeta
-  BEGIN
-    DBMS_OUTPUT.PUT_LINE('CASO ACTUALIZACION INCORRECTA');
-    SELECT tarjeta_id INTO v_tarjeta_id
-    FROM tarjeta
-    WHERE numero_tarjeta = 1122334455667788;
-
-    UPDATE tarjeta
-    SET mes = 01, anio = 19
-    WHERE tarjeta_id = v_tarjeta_id;
-  EXCEPTION
-    WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error en la actualizacion incorrecta: ' || SQLERRM);
+      DBMS_OUTPUT.PUT_LINE('Excepción correcta');
   END;
 END;
 /
