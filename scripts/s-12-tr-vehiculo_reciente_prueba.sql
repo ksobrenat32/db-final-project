5fb06a90fd8ca2e0d741e3affd47178dfffea1cd
ksobrenat32 <ksobrenatural@tuta.io>
2024-12-08 12:59:57 -0600

    Fix s-11 tr-vehiculo_reciente

--- scripts/s-12-tr-vehiculo_reciente_prueba.sql
+++ scripts/s-12-tr-vehiculo_reciente_prueba.sql
@@ -1,3 +1,7 @@
+--@Autor(es): Enrique Job Calderón Olalde <@ksobrenat32>, Erick Nava Santiago
+--@Fecha creación: 1 de diciembre de 2024
+--@Descripción: Prueba del trigger tr_vehiculo_reciente
+
 CONNECT cn_proy_admin/cn_proy_admin@//localhost:1521/FREEPDB1
 SET SERVEROUTPUT ON;
 
@@ -7,6 +11,7 @@ BEGIN
   -- Caso 1: Inserción correcta
   BEGIN
     DBMS_OUTPUT.PUT_LINE('CASO 1: Inserción correcta');
+
     -- Obteniendo un usuario conductor
     SELECT MAX(usuario_id) INTO v_usuario_id
     FROM usuario
@@ -16,6 +21,7 @@ BEGIN
     INSERT INTO vehiculo(vehiculo_id, placas, anio, modelo_id, usuario_id)
     VALUES (vehiculo_seq.NEXTVAL, '77RAUP1', 2020, 1, v_usuario_id);
 
+    DBMS_OUTPUT.PUT_LINE('Inserción correcta');
   EXCEPTION
     WHEN OTHERS THEN
       DBMS_OUTPUT.PUT_LINE('Error en el caso 1: ' || SQLERRM);
@@ -33,9 +39,10 @@ BEGIN
     INSERT INTO vehiculo(vehiculo_id, placas, anio, modelo_id, usuario_id)
     VALUES (vehiculo_seq.NEXTVAL, '77RAUP2', 2002, 1, v_usuario_id);
 
+    DBMS_OUTPUT.PUT_LINE('Error en la inserción incorrecta: Se pudo insertar el vehículo');
   EXCEPTION
     WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error en el caso 2: ' || SQLERRM);
+      DBMS_OUTPUT.PUT_LINE('Excepción correcta');
   END;
 
   -- Caso 3: Actualización correcta
@@ -51,6 +58,7 @@ BEGIN
     SET anio = 2023
     WHERE usuario_id = v_usuario_id;
 
+    DBMS_OUTPUT.PUT_LINE('Actualización correcta');
   EXCEPTION
     WHEN OTHERS THEN
       DBMS_OUTPUT.PUT_LINE('Error en el caso 3: ' || SQLERRM);
@@ -69,9 +77,10 @@ BEGIN
     SET anio = 1990
     WHERE usuario_id = v_usuario_id;
 
+    DBMS_OUTPUT.PUT_LINE('Error en la actualización incorrecta: Se pudo actualizar el vehículo');
   EXCEPTION
     WHEN OTHERS THEN
-      DBMS_OUTPUT.PUT_LINE('Error en el caso 4: ' || SQLERRM);
+      DBMS_OUTPUT.PUT_LINE('Excepción correcta');
   END;
 END;
 /
